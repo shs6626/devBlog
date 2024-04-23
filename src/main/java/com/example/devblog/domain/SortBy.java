ab7fd73ad5cc3f7456cdd402b112373a69b0bbd0
sayya <djdj9876@naver.com>
2024-04-23 09:32:20 +0900

    fix: paging 로직에 문제 있어서 수정

--- src/main/java/com/example/devblog/domain/SortBy.java
+++
@@ -1,5 +0,0 @@
-package com.example.devblog.domain;
-
-public enum SortBy {
-    CREATED_AT, UPDATED_AT, TITLE
-}
--- src/main/java/com/example/devblog/utils/paging/PagingUtils.java
+++ src/main/java/com/example/devblog/utils/paging/PagingUtils.java
@@ -9,14 +9,14 @@ public class PagingUtils {
         int totalPageBlockCnt = calTotalPageBlockCnt(totalRowDataCnt, pageSize);
         int currentPage = checkCurrentPageValidation(Integer.parseInt(currentPage_), totalPageBlockCnt);
         /* STEP 2 */
-        int endPageBlock = calEndPageBlock(currentPage, pageBlockCnt, totalPageBlockCnt);
+        int endPageBlock = calEndPageBlock(currentPage, pageBlockCnt);
         int startPageBlock = calStartPageBlock(endPageBlock, pageBlockCnt);
         // 유효성 검사를 더 늦게 해줘야 함
-        endPageBlock = checkEndPageBlockValidation(currentPage, pageBlockCnt);
-        boolean next = (endPageBlock == totalPageBlockCnt);
+        endPageBlock = checkEndPageBlockValidation(endPageBlock, pageBlockCnt);
+        boolean next = (currentPage != totalPageBlockCnt);
 
-        startPageBlock = checkStartPageBlockValidation(endPageBlock);
-        boolean prev = (startPageBlock == 1);
+        startPageBlock = checkStartPageBlockValidation(startPageBlock);
+        boolean prev = (currentPage != 1);
         /* STEP 3 */
         int startRowDataNum = (currentPage-1) * pageSize + 1;
         int endRowDataNum = startRowDataNum + pageSize -1;
@@ -33,10 +33,8 @@ public class PagingUtils {
         );
     }
 
-    // pageSize(한 페이지 당 보여줄 entity 개수) -- 몇 개 보여줄 것인지 개발자가 정하고 나머지는 디폴트 값을 뿌리기
     private static int checkPageSizeValidation(String pageSize) {
-        // pageSize 조절 -- default값은 10
-        if ("10".equals(pageSize) || "30".equals(pageSize) || "50".equals(pageSize)) {
+        if ("5".equals(pageSize) || "10".equals(pageSize) || "30".equals(pageSize) || "50".equals(pageSize)) {
             return Integer.parseInt(pageSize);
         }
         return 10;
@@ -58,7 +56,7 @@ public class PagingUtils {
         return (int)(totalRowDataCnt + pageSize - 1) / pageSize;
     }
 
-    private static int calEndPageBlock(int currentPage, int pageBlockCnt , int totalPageBlockCnt) {
+    private static int calEndPageBlock(int currentPage, int pageBlockCnt) {
         return ((currentPage + pageBlockCnt - 1) / pageBlockCnt) * pageBlockCnt;
     }
 
@@ -66,8 +64,8 @@ public class PagingUtils {
         return Math.min(endPageBlock, totalPageBlockCnt);
     }
 
-    private static int calStartPageBlock(int endPageBlock, int totalPageBlockCnt) {
-        return endPageBlock - totalPageBlockCnt + 1;
+    private static int calStartPageBlock(int endPageBlock, int pageBlockCnt) {
+        return endPageBlock - pageBlockCnt + 1;
     }
 
     private static int checkStartPageBlockValidation(int startPageBlock) {
